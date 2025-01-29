88a079b823a0fdde4660b9a37f05e1581e771694
ohyaju <bnomin257@gmail.com>
2025-01-29 10:06:45 +0800

    data uurchlult bolgosn

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,73 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
-function Card() {
+const users = [
+{
+firstName:"ursa",
+lastName: "ursa",
+role: "developer",
+phoneNumber: "88968896",
+email:"ursursa@gmail.com",
+adress: "BZD 1khoroo 5-10",
+companyName:"Meta",
+},
+{
+  firstName:"baljan",
+  lastName: "baljan",
+  role: "developer",
+  phoneNumber: "96567628",
+  email:"baljukk@gmail.com",
+  adress: "BZD 3khoroo 75-12",
+  companyName:"ONDO",
+},
+{
+  firstName:"chinguun",
+  lastName: "chinguun",
+  role: "developer",
+  phoneNumber: "90178777",
+  email:"chinguun@gmail.com",
+  adress: "SBD 12khoroo 6-1",
+  companyName:"Pranzo",
+},
+{firstName:"ariunjargal",
+  lastName: "ariunjargal",
+  role: "developer",
+  phoneNumber: "88674545",
+  email:"ariunjargal@gmail.com",
+  adress: "KHUD 9khoroo 100-8",
+  companyName:"Tesla",
+}
+]
+function Card(props) {
+  console.log(props);
   return (
     <div>
       <div className="cards">
         <div className="name-flex">
-          <p className="Jhon">Oyunbat</p>
-          <p className="Doe"> Oyunbat</p>
+          <p className="firstName">{props.user.firstName}</p>
+          <p className="lastName">{props.user.lastName}</p>
         </div>
-        <p className="job">Designer</p>
+        <p className="role">{props.user.role}</p>
 
         <div className="address-flex">
           <div>
             <img src="phone.svg"></img>
-            <span> 90098070</span>
+            <span>{props.user.phoneNumber}</span>
           </div>
           <div>
             <img src="email.svg"></img>
-            <span> email@oyunbatdomain.com</span>
+            <span className="email">{props.user.email}</span>
           </div>
           <div>
             <img src="Group.svg"></img>
-            <span>Your address goes here <br />
-              125 Street, USA</span>
+            <span className="adress">{props.user.adress
+              } <br />
+</span>
           </div>
         </div>
         <img className="shape" src="Shape.png"></img>
 
         <img className="logo" src="Logo.svg"></img>
-        <p className="co-name">Pinecone Academy </p>
+        <p className="companyName">Pinecone Academy </p>
 
         <img className="qr" src="QR.svg"></img>
 
@@ -41,8 +79,8 @@ function Card() {
 }
 export default function Home() {
   const cards = [];
-  for (let i = 0; i < 18; i++) {
-    cards.push(<Card></Card>)
+  for (let i = 0; i < users.length; i++) {
+    cards.push(<Card user={users[i]}></Card>)
   }
   return (<div>{cards}</div>
 
